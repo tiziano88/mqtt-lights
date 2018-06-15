472f56e572fadd8e2d78c18bdd13729e9a1afabd
Tiziano Santoro <tiziano88@gmail.com>
2018-06-15 18:21:42 +0100

    Rain effect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate getopts;
+extern crate mote;
+extern crate palette;
+extern crate rand;
+extern crate rgb;
 extern crate rumqtt;
 extern crate serde;
 extern crate serde_json;
@@ -5,6 +10,9 @@ extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use palette::blend::Blend;
+use palette::Mix;
+use rand::distributions::Distribution;
 use rumqtt::{MqttCallback, MqttClient, MqttOptions, QoS};
 use std::sync::{Arc, RwLock};
 use std::thread;
@@ -19,11 +27,45 @@ trait Device {
     fn state(&mut self) -> Vec<rumqtt::Message>;
 }
 
+const BACKGROUND: palette::rgb::LinSrgb = palette::rgb::LinSrgb {
+    red: 0.0,
+    green: 0.0,
+    blue: 0.0,
+    standard: std::marker::PhantomData,
+};
+
+#[derive(Debug)]
+struct Particle {
+    creation_time: u64,
+    color: palette::rgb::LinSrgb,
+}
+
+#[derive(Debug)]
+struct Segment {
+    particles: Vec<Particle>,
+    pixels: Vec<palette::rgb::LinSrgb>,
+}
+
+impl Default for Segment {
+    fn default() -> Self {
+        Segment {
+            particles: vec![],
+            pixels: [BACKGROUND; mote::PIXELS_PER_CHANNEL as usize].to_vec(),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Light {
     pub name: String,
     pub id: String,
     pub brightness: u8,
+
+    lambda: u8,
+    decay: u8,
+    rate: u8,
+
+    segments: Vec<Segment>,
 }
 
 impl Light {
@@ -46,6 +88,16 @@ impl Light {
     }
     fn brightness_state_topic(&self) -> String {
         format!("home/{}/brightness/status", self.id)
+    }
+
+    fn lambda_command_topic(&self) -> String {
+        format!("home/{}/lambda/set", self.id)
+    }
+    fn decay_command_topic(&self) -> String {
+        format!("home/{}/decay/set", self.id)
+    }
+    fn rate_command_topic(&self) -> String {
+        format!("home/{}/rate/set", self.id)
     }
 }
 
@@ -83,6 +135,15 @@ impl Device for Light {
             println!("rgb");
         } else if topic == self.brightness_command_topic() {
             println!("brightness");
+        } else if topic == self.lambda_command_topic() {
+            println!("lambda");
+            self.lambda = val.parse().unwrap();
+        } else if topic == self.decay_command_topic() {
+            println!("decay");
+            self.decay = val.parse().unwrap();
+        } else if topic == self.rate_command_topic() {
+            println!("rate");
+            self.rate = val.parse().unwrap();
         } else {
             println!("other");
         };
@@ -108,17 +169,44 @@ struct LightConfig {
 }
 
 fn main() {
+    let mut opts = getopts::Options::new();
+    opts.optopt("", "mqtt_broker_address", "", "");
+    opts.optopt("", "mqtt_broker_username", "", "");
+    opts.optopt("", "mqtt_broker_password", "", "");
+    let matches = opts.parse(std::env::args()).unwrap();
+
     let light = Arc::new(RwLock::new(Light {
         name: "bedroom lights".to_string(),
         id: "bedroom/ceiling".to_string(),
         brightness: 0,
+
+        lambda: 128,
+        decay: 128,
+        rate: 128,
+
+        segments: vec![
+            Segment::default(),
+            Segment::default(),
+            Segment::default(),
+            Segment::default(),
+        ],
     }));
 
-    let client_options = MqttOptions::new()
+    let mut client_options = MqttOptions::new()
         .set_keep_alive(5)
         .set_reconnect(3)
         .set_client_id("rumqtt-docs")
         .set_broker("127.0.0.1:1883");
+
+    if let Some(mqtt_address) = matches.opt_str("mqtt_broker_address") {
+        client_options = client_options.set_broker(&mqtt_address);
+    }
+    if let Some(mqtt_username) = matches.opt_str("mqtt_broker_username") {
+        client_options = client_options.set_user_name(&mqtt_username);
+    }
+    if let Some(mqtt_password) = matches.opt_str("mqtt_broker_password") {
+        client_options = client_options.set_password(&mqtt_password);
+    }
 
     let handler_light = light.clone();
     let cb = MqttCallback::new()
@@ -150,6 +238,7 @@ fn main() {
         .expect("Could not publish");
 
     {
+        /*
         let client = client.clone();
         let interval = Duration::from_secs(5);
         thread::spawn(move || loop {
@@ -166,8 +255,92 @@ fn main() {
                 .expect("Could not publish");
             thread::sleep(interval);
         });
+        */
+
+        let background_light = light.clone();
+        thread::spawn(move || {
+            println!("start");
+            let mut mote = mote::Mote::new("/dev/ttyACM0", true);
+            mote.clear();
+
+            let mut rng = rand::thread_rng();
+
+            let mut n = 0u64;
+            loop {
+                let mut interval = std::time::Duration::from_millis(50);
+                {
+                    let mut light = background_light.write().unwrap();
+                    interval = std::time::Duration::from_millis(1000 / light.rate as u64);
+
+                    let decay = 1.0 * light.decay as f32 / 255.0;
+
+                    let dist = rand::distributions::Poisson::new(2.0 * light.lambda as f64);
+                    for i in 0..light.segments.len() {
+                        let mut segment = &mut light.segments[i];
+                        if dist.sample(&mut rng) > 1 {
+                            segment.particles.push(Particle {
+                                creation_time: n,
+                                color: random_color(),
+                            });
+                        }
+                        let mask = make_mask(&segment.particles, n);
+                        for i in 0..mote::PIXELS_PER_CHANNEL as usize {
+                            segment.pixels[i] = segment.pixels[i].screen(mask[i]);
+                            segment.pixels[i] = segment.pixels[i].mix(&BACKGROUND, decay);
+                        }
+                    }
+
+                    let pixels = light
+                        .segments
+                        .iter()
+                        .flat_map(|x| x.pixels.clone())
+                        .collect::<Vec<_>>();
+                    mote.write(&to_array(&pixels.iter().map(to_rgb).collect::<Vec<_>>()));
+                }
+                std::thread::sleep(std::time::Duration::from_millis(50));
+                n += 1;
+            }
+        });
     }
 
     let forever = std::time::Duration::new(std::u64::MAX, 0);
     std::thread::sleep(forever);
+}
+
+fn to_array(pixels: &[rgb::RGB8]) -> [rgb::RGB8; mote::TOTAL_PIXELS] {
+    let mut out = [to_rgb(&BACKGROUND); mote::TOTAL_PIXELS];
+    for i in 0..pixels.len() {
+        out[i] = pixels[i];
+    }
+    out
+}
+
+fn make_mask(particles: &Vec<Particle>, n: u64) -> [palette::rgb::LinSrgb; mote::TOTAL_PIXELS] {
+    // Speed in pixels per cycle.
+    const SPEED: f32 = 0.5;
+    let mut mask = [BACKGROUND; mote::TOTAL_PIXELS];
+    for p in particles.iter() {
+        let x = ((n - p.creation_time) as f32 * SPEED) as usize;
+        if x < mask.len() {
+            mask[x] = p.color;
+        }
+    }
+    mask
+}
+
+fn random_color() -> palette::rgb::LinSrgb {
+    let between = rand::distributions::Uniform::new(0, 360);
+    let mut rng = rand::thread_rng();
+    let h = palette::RgbHue::<f32>::from_degrees(between.sample(&mut rng) as f32);
+    let s = 1.0;
+    let v = 0.5;
+    palette::Hsv::new(h, s, v).into()
+}
+
+fn to_rgb(c: &palette::rgb::LinSrgb) -> rgb::RGB8 {
+    rgb::RGB8 {
+        r: (c.red * 255.0) as u8,
+        g: (c.green * 255.0) as u8,
+        b: (c.blue * 255.0) as u8,
+    }
 }
